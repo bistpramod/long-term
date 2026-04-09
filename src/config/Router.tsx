64632bb4e01be70e1e48a8d3a79d096e77628ded
Pra <155552124+bistpramod@users.noreply.github.com>
2026-04-09 23:37:40 +0545

    Update Router.tsx

--- src/config/Router.tsx
+++ src/config/Router.tsx
@@ -1,63 +1,14 @@
+
 import { createBrowserRouter, RouterProvider } from "react-router";
-// import { BrowserRouter, Route, Routes } from "react-router";
-import HomePage from "../pages/home/HomePage";
-import ForgetPassword from "../pages/auth/ForgetPassword";
 import NotFound from "../pages/error/NotFound";
-import UserList from "../pages/admin/user/UserList";
-import AdminLayout from "../pages/layouts/AdminLayout";
-import AdminDashboard from "../pages/admin/Dashboard";
-import UserLayout from "../pages/layouts/UserLayout";
-import UserDashboard from "../pages/user/UserDashboard";
-import UserProfile from "../pages/user/UserProfile";
-import UserRegister from "../pages/admin/user/UserRegister";
+import { AdminRouter } from "../lib/router/admin-router";
+import { UserRouter } from "../lib/router/user-router";
+import { PublicRouter } from "../lib/router/public-router";
 
 const routerData = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/forget-password", Component: ForgetPassword },
-
-  { path: "/admin", element: <AdminLayout />, children: [
-    { index: true, Component: AdminDashboard},
-    
-    { path: "users", element: <UserList />} ,   // user listing table 
-    // task Update for tomorrow
-    { path: "user/create", element: <UserRegister />},  // User form
-    { path: "user/userId/detail", element:<>User Detail</>},
-    
-      // user edit 
-      // user detail,
-      // user delete 
-    // pos
-    // Product 
-      // product list, 
-      // product create 
-        // name, description, price, discount, category, brand, code, stock, images, 
-        // createdBy, updatedBy, vendor, sku, status ()
-      // product Detail 
-      // product Edit.
-      // product delete
-    // Order 
-      // order list 
-      // Order Order 
-      // Order Detil 
-      // order edit 
-      // order delete 
-    // inventory
-      // Stock View 
-      // stock update 
-    // Transactions 
-      // list 
-      // view 
-  ]},
-
-
-  { path: "/user", element: <UserLayout />, children:[
-    { index: true, element: <UserDashboard />},
-    { path:"profile", element: <UserProfile />}
-  ]},
-
-  
-  
-  
+  ...PublicRouter,
+  ...AdminRouter,
+  ...UserRouter,
   // Design a 404 not found page
   { path: "*", element: <NotFound /> },
 ]);
