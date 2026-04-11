8d1ad7d001d5765477762299f8105b2edc03390c
Pra <155552124+bistpramod@users.noreply.github.com>
2026-04-11 22:25:09 +0545

    Integrate AdminSidebar and user authentication
    Added AdminSidebar component and integrated user authentication to display user names dynamically.

--- src/pages/layouts/AdminLayout.tsx
+++ src/pages/layouts/AdminLayout.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router";
 import Logo from "../../components/logo/Logo";
 import { LuCircleUserRound } from "react-icons/lu";
+import { AdminSidebar } from "../../components/ui/sidebar/AdminSidebar";
+import { useAuth } from "../../lib/hooks/useAuth";
 
 export default function AdminLayout() {
+  // auth 
+  const {authUser} = useAuth()
   return (
     <>
       <section className="w-full bg-gray-100 h-screen">
@@ -21,7 +25,9 @@ export default function AdminLayout() {
                   className="flex items-center gap-3 text-sm font-medium transition-colors focus:relative hover:cursor-pointer"
                 >
                   <LuCircleUserRound className="size-7" />
-                  <span className="font-semibold">Sandesh Bhattarai</span>
+                  <span className="font-semibold">
+                    {authUser?.firstName} {authUser?.lastName}
+                  </span>
                 </button>
               </span>
 
@@ -65,158 +71,11 @@ export default function AdminLayout() {
         </header>
 
         <main className="flex w-full gap-3">
-          <aside className="w-64">
-            <div className="flex h-[95vh] flex-col justify-between border-e border-gray-100 bg-white">
-              <div className="p-4">
-                <ul className="space-y-1">
-                  <li>
-                    <a
-                      href="#"
-                      className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
-                    >
-                      General
-                    </a>
-                  </li>
+          {/* {
+            (loggedInUser.role === 'admin') ? <AdminSidebar /> : <></>
+          } */}
+          <AdminSidebar loggedInUser={authUser} />
 
-                  <li>
-                    <details className="group [&amp;_summary::-webkit-details-marker]:hidden">
-                      <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
-                        <span className="text-sm font-medium"> Teams </span>
-
-                        <span className="shrink-0 transition duration-300 group-open:-rotate-180">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="size-5"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <path
-                              fill-rule="evenodd"
-                              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                              clip-rule="evenodd"
-                            ></path>
-                          </svg>
-                        </span>
-                      </summary>
-
-                      <ul className="mt-2 space-y-1 px-4">
-                        <li>
-                          <a
-                            href="#"
-                            className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                          >
-                            Banned Users
-                          </a>
-                        </li>
-
-                        <li>
-                          <a
-                            href="#"
-                            className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                          >
-                            Calendar
-                          </a>
-                        </li>
-                      </ul>
-                    </details>
-                  </li>
-
-                  <li>
-                    <a
-                      href="#"
-                      className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                    >
-                      Billing
-                    </a>
-                  </li>
-
-                  <li>
-                    <a
-                      href="#"
-                      className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                    >
-                      Invoices
-                    </a>
-                  </li>
-
-                  <li>
-                    <details className="group [&amp;_summary::-webkit-details-marker]:hidden">
-                      <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
-                        <span className="text-sm font-medium"> Account </span>
-
-                        <span className="shrink-0 transition duration-300 group-open:-rotate-180">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="size-5"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <path
-                              fill-rule="evenodd"
-                              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                              clip-rule="evenodd"
-                            ></path>
-                          </svg>
-                        </span>
-                      </summary>
-
-                      <ul className="mt-2 space-y-1 px-4">
-                        <li>
-                          <a
-                            href="#"
-                            className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                          >
-                            Details
-                          </a>
-                        </li>
-
-                        <li>
-                          <a
-                            href="#"
-                            className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                          >
-                            Security
-                          </a>
-                        </li>
-
-                        <li>
-                          <a
-                            href="#"
-                            className="w-full rounded-lg px-4 py-2 [text-align:inherit] text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                          >
-                            Logout
-                          </a>
-                        </li>
-                      </ul>
-                    </details>
-                  </li>
-                </ul>
-              </div>
-
-              <div className="sticky inset-x-0 bottom-0 border-t border-gray-100">
-                <a
-                  href="#"
-                  className="flex items-center gap-2 bg-white p-4 hover:bg-gray-50"
-                >
-                  <img
-                    alt=""
-                    src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?auto=format&amp;fit=crop&amp;q=80&amp;w=1160"
-                    className="size-10 rounded-full object-cover"
-                  />
-
-                  <div>
-                    <p className="text-xs">
-                      <strong className="block font-medium">
-                        Eric Frusciante
-                      </strong>
-
-                      <span> eric@frusciante.com </span>
-                    </p>
-                  </div>
-                </a>
-              </div>
-            </div>
-          </aside>
           <section className="w-full bg-gray-200 p-3 mt-3 rounded-md">
             <Outlet />
           </section>
